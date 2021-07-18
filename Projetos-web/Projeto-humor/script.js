5d36daa91805f0605e4bf06a96b3c7e700beb063
mdar4 <darasilva2391@gmail.com>
2021-07-18 02:19:11 -0300

    adicionando mais estados

--- Projetos-web/Projeto-humor/script.js
+++ Projetos-web/Projeto-humor/script.js
@@ -3,15 +3,45 @@ var mudaHumor = document.getElementById('humor');
 
 btn.onclick = function() {
 
-    if(btn.value === 'Feliz'){
-        mudaHumor.src = '/img/mobsorrindo.jpg'
-        btn.value = 'Sério';
-        btn.innerHTML = 'agora estou feliz';
+    if(btn.value === 'primeiro'){
+        mudaHumor.src = '/img/mobsorrindo.jpg';
+        btn.value = 'segundo';
+        btn.innerHTML = 'Feliz';
+
+    } else if (btn.value === 'segundo') {
+        mudaHumor.src = '/img/mob10.jpg';
+        btn.value = 'terceiro';
+        btn.innerHTML = '20%';
+
+    } else if (btn.value === 'terceiro') {
+        mudaHumor.src = '/img/mob15.jpg';
+        btn.value = 'quarto';
+        btn.innerHTML = '35%';
+    
+    } else if (btn.value === 'quarto'){
+        mudaHumor.src = '/img/mob20.jpg';
+        btn.value = 'quinto';
+        btn.innerHTML = '50%';
+
+    } else if (btn.value === 'quinto'){
+        mudaHumor.src = '/img/mob80.jpg';
+        btn.value = 'sexto';
+        btn.innerHTML = '80%';
+
+    } else if (btn.value === 'sexto'){
+        mudaHumor.src = '/img/mobdd.jpg';
+        btn.value = 'setimo';
+        btn.innerHTML = '???';
+    
+    } else if (btn.value === 'setimo'){
+        mudaHumor.src = '/img/mobcansad.jpg';
+        btn.value = 'oitavo';
+        btn.innerHTML = 'Cansado'
 
     } else {
-        mudaHumor.src = '/img/images.jpg'
-        btn.value = 'Feliz'
-        btn.innerHTML = 'Mude meu humor'
+        mudaHumor.src = '/img/images.jpg';
+        btn.value = 'primeiro';
+        btn.innerHTML = '0%';
     }
     
 }
